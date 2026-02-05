ae16d3f4c61c515018bb5ecdbb95e8dbd9dfbb2e
Nate-saikai <naseky1000@gmail.com>
2026-02-05 08:11:29 +0800

    Fully fledged Application ver 1 - Everything works except:      - checkout()      - No roles

--- src/main/java/com/capstone/bookstore/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.bookstore.repositories;
-
-import com.capstone.bookstore.models.OrderHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<OrderHistory, Long> {
-}
